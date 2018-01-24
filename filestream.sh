98d0cc645ce725f8154dd2b96e4409eee69661ff
IoTPi <pi@IoTPi>
2018-01-23 18:52:26 -0600

    Client must start listening before stream starts

---
+++ filestream.sh
@@ -0,0 +1,16 @@
+IP=$1
+FILE=$2
+
+if [ -z $1 ]
+then
+    echo "Please specify an IP!"
+    exit
+fi
+
+if [ -z $2 ]
+then
+    echo "Please specify a h264 file!"
+    exit
+fi
+
+gst-launch-1.0 -v filesrc location=$FILE ! qtdemux ! video/x-h264 ! h264parse ! rtph264pay config-interval=1 ! udpsink host=$IP port=9001
