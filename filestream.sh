029e8cee2a99a251d9b6a2e41d73bd1fe3024a5b
IoTPi <pi@IoTPi>
2018-01-25 13:49:36 -0600

    Sleep before starting stream to allow client to start listening

--- filestream.sh
+++ filestream.sh
@@ -1,5 +1,6 @@
 IP=$1
 FILE=$2
+SLEEP=$3
 
 if [ -z $1 ]
 then
@@ -11,6 +12,10 @@ if [ -z $2 ]
 then
     echo "Please specify a h264 file!"
     exit
+fi
+
+if [ $SLEEP = 'y' ]
+then sleep 2
 fi
 
 gst-launch-1.0 -v filesrc location=$FILE ! qtdemux ! video/x-h264 ! h264parse ! rtph264pay config-interval=1 ! udpsink host=$IP port=9001
