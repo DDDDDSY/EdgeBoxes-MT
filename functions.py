015145ed98e8981b83f4453a2c49ca62d0078a4d
IoTPi <pi@IoTPi>
2018-02-19 12:44:20 -0600

    Fixed xy coordinate order

--- functions.py
+++ functions.py
@@ -4,6 +4,6 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 def draw_boxes(boxes, frame):
 
     for box in boxes:
-        frame2 = cv2.rectangle(frame, (box[0], box[2]), (box[1], box[3]), (255,255,0), 3)
+        frame2 = cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255,255,0), 3)
 
     return frame2
