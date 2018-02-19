31ceda86bc517c7ca1cba4a6fa0fcd218374c6c6
IoTPi <pi@IoTPi>
2018-02-18 19:23:57 -0600

    Frame by frame processing with no multithreading

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import cv2
 import numpy as np
+import threading
+import time
+
 from classes import reader
 
 file = "video.mp4"
 modelfile = "model.yml.gz" #StructuredEdgeDetection model (generates edgemap)
 
 video = reader(file)
-video.read()
 
 print("Loading model...")
 edgeGenerator = cv2.ximgproc.createStructuredEdgeDetection(model = modelfile)
 
-edgearray = np.zeros((video.height, video.width), dtype=np.float32) #Empty array for edgemap
-print("Detecting...")
-edgearray = edgeGenerator.detectEdges(video.frame)
+frames = 0
+total_fps = 0
 
-cv2.imshow('image', edgearray)
-cv2.waitKey(10000)
+try:
+  while True:
+
+    beginning = time.time() #For FPS calculations
+
+    video.read()
+    edgearray = np.zeros((video.height, video.width), dtype=np.float32) #Empty array for edgemap
+    edgearray = edgeGenerator.detectEdges(video.frame)
+
+    fps = 1/(time.time()-beginning)
+    print("FPS: ", fps)
+
+    frames = frames + 1
+    total_fps = total_fps + fps
+
+except KeyboardInterrupt:
+    exit(total_fps/frames)
