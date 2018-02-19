cc8598953fa9ff7d0fcd2da47ce83aac26650306
IoTPi <pi@IoTPi>
2018-02-18 19:32:05 -0600

    Replaced ready logic with a frame buffer

--- classes.py
+++ classes.py
@@ -7,17 +7,23 @@ class reader:
         self.video_capture = cv2.VideoCapture(filename)
         self.width = int(self.video_capture.get(3))
         self.height = int(self.video_capture.get(4))
-        self.ready = False #Boolean to indicate whether frame is ready
+        self.execute = False
+
+        #Prepare frame buffer
+        _, self.frame = self.video_capture.read()
+        self.frame = self.frame.astype(np.float32)
+        self.frame = np.apply_along_axis(self.normalize, 0, self.frame)
 
     def normalize(self, a): #Normalize to a float in [0,1]
         return np.float32(a/255)
 
     def read(self):
-        self.ready = False
+        while not self.execute: continue #wait for main thread
+        self.currentframe = self.frame
         _, self.frame = self.video_capture.read()
         if self.frame is None:
             exit("End of video file!")
 
         self.frame = self.frame.astype(np.float32) #Change type to be compatible with edgeDetect
         self.frame = np.apply_along_axis(self.normalize, 0, self.frame) #Apply normalize function over array
-        self.ready = True
+        self.execute = False
