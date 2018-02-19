2d845ed4cc8757efa3afe6b87c646153b578cad7
IoTPi <pi@IoTPi>
2018-02-18 18:48:24 -0600

    Rearranged to be more logical, added helpful comments

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ modelfile = "model.yml.gz" #StructuredEdgeDetection model (generates edgemap)
 
 img = cv2.imread(imagefile)
 imgarray = img.astype(np.float32) #Change type to be compatible with edgeDetect
-
-def normalize(a):
-    return np.float32(a/255)
-
-imgarray = np.apply_along_axis(normalize, 0, imgarray)
 h, w, d = imgarray.shape
+
+def normalize(a): #Normalize to a float in [0,1]
+    return np.float32(a/255)
+imgarray = np.apply_along_axis(normalize, 0, imgarray) #Apply normalize function over array
 
 print("Loading model...")
 edgeGenerator = cv2.ximgproc.createStructuredEdgeDetection(model = modelfile)
