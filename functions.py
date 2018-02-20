133d447889916d5932608753b68e492ea0cfcba1
IoTPi <pi@IoTPi>
2018-02-19 22:45:00 -0600

    Updated function definition

--- functions.py
+++ functions.py
@@ -3,7 +3,9 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 def draw_boxes(boxes, frame):
 
+    frame2 = frame.copy()
+
     for box in boxes:
-        frame2 = cv2.rectangle(frame, (box[0], box[1]), (box[0]+box[2], box[1]+box[3]), (255,255,0), 3)
+        frame2 = cv2.rectangle(frame2, (box[0]-1, box[1]-1), (box[0]+box[2]-1, box[1]+box[3]-1), (255,255,0), 2)
 
     return frame2
