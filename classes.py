a5c9d8e6ef370f7d019bb07167909e0f7fc84d3d
Josh <bluedude227@gmail.com>
2018-03-06 16:47:00 -0600

    Implemented multithreading for box prediction

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from multiprocessing import Process, Queue
+import math
 
 class reader:
 
@@ -30,6 +31,7 @@ class reader:
         self.frame = self.frame.astype(np.float32) #Change type to be compatible with edgeDetect
         self.frame = np.divide(self.frame, 255.0) #Normalize to [0,1]
 
+        
 class generator:
 
     def __init__(self, modelfile, video, num_threads):
@@ -37,7 +39,7 @@ class generator:
         self.Reader = video #multimedia reader
         self.Reader.execute = True
         self.framenum = 0 #keep track of how many frames have been processed
-        
+
         print("Loading model...")
         self.edgeGenerator = cv2.ximgproc.createStructuredEdgeDetection(model = modelfile)
 
@@ -45,17 +47,17 @@ class generator:
         self.queue = list()
         for x in range(0, num_threads):
 	        self.queue.append(Queue())
-        
+
         ### INITIALIZE THREADS ###
         self.thread = list()
         for x in range(0, num_threads):
+            while self.framenum >= self.Reader.framenum: continue #wait for frame
             self.thread.append(Process(target=self._generate,
                                args=(self.edgeGenerator,
                                      self.Reader.currentframe,
                                      self.queue[x],),
                                daemon = True))
-            print("Thread", x, " Initializing")
-            while self.framenum >= self.Reader.framenum: continue
+            print("Generation Thread", x, " Initializing")
             self.thread[x].start()
             self.framenum = self.framenum + 1
             self.Reader.execute = True
@@ -92,6 +94,78 @@ class generator:
         suppressed_edgearray = edgeGenerator.edgesNms(edgearray, orientationarray)
         q.put(suppressed_edgearray)
         q.put(orientationarray)
+        
+    def next_thread(self):
+        if self.threadnum == self.num_threads - 1:
+            return 0
+        else:
+            return self.threadnum + 1
+
+
+
+### Class to generate boxes in a multithreaded environment. Transparent to main ###
+class predictor:
+
+    def __init__(self, generator):
+
+        self.generator = generator
+        self.generator.execute = True #prepare current_edge*
+    
+        self.num_threads = math.floor(self.generator.num_threads/2) #2 gen. threads per pred. thread
+        
+        self.framenum = 0 #keep track of how many frames have been processed
+        
+        self.boxGenerator = cv2.ximgproc.createEdgeBoxes(maxBoxes = 100,
+                                                    alpha = 0.65,
+                                                    beta = 0.75,
+                                                    minScore = 0.03)
+
+        #Create a queue for each thread
+        self.queue = list()
+        for x in range(0, self.num_threads):
+            self.queue.append(Queue())
+
+        ### INITIALIZE THREADS ###
+        self.thread = list()
+        for x in range(0, self.num_threads):
+            while self.generator.execute: continue #wait for edgemap
+            self.thread.append(Process(target=self._predict,
+                               args=(self.boxGenerator,
+                                     self.generator.current_edgearray,
+                                     self.generator.current_orientationarray,
+                                     self.queue[x],),
+                               daemon = True))
+            print("Prediction Thread", x, "Initializing")
+            self.thread[x].start()
+            self.generator.execute = True
+            self.framenum = self.framenum + 1
+        
+        self.threadnum = 0
+        self.boxes = Queue()
+
+    
+    def predict(self):
+      while True:
+        
+        self.boxes.put(self.queue[self.threadnum].get())
+        
+        while self.generator.execute: continue #Wait for generator
+        self.thread[self.threadnum] = Process(target=self._predict,
+                                              args=(self.boxGenerator,
+                                                    self.generator.current_edgearray,
+                                                    self.generator.current_orientationarray,
+                                                    self.queue[self.threadnum],),
+                                              daemon = True)
+        self.thread[self.threadnum].start()
+        self.generator.execute = True
+        self.framenum = self.framenum + 1
+        self.threadnum = self.next_thread()
+
+        
+    def _predict(self, boxgenerator, edgearray, orientationarray, q):
+        boxes = boxgenerator.getBoundingBoxes(edgearray, orientationarray)
+        q.put(boxes)
+
         
     def next_thread(self):
         if self.threadnum == self.num_threads - 1:
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import threading
 from multiprocessing import Process, Queue
 import time
 
-from classes import reader, generator
+from classes import reader, generator, predictor
 from functions import draw_boxes
 
 file = "video.mp4"
@@ -15,52 +15,28 @@ videoThread = threading.Thread(target=video.read)
 videoThread.daemon = True
 videoThread.start()
 
-num_threads = 3
+num_threads = 6
 Generator = generator(modelfile, video, num_threads)
 generatorThread = threading.Thread(target=Generator.generate)
 generatorThread.daemon = True
 generatorThread.start() #starts with first maps ready
 
-boxGenerator = cv2.ximgproc.createEdgeBoxes(maxBoxes = 100,
-                                            alpha = 0.65,
-                                            beta = 0.75,
-                                            minScore = 0.03)
-
-def boundingBoxes(boxgenerator, edgearray, orientationarray, q):
-    boxes = boxgenerator.getBoundingBoxes(edgearray, orientationarray)
-    q.put(boxes)
-
-queue = Queue() #queue for boxes
+Predictor = predictor(Generator)
+predictorThread = threading.Thread(target = Predictor.predict)
+predictorThread.daemon = True
+predictorThread.start()
 
 frames = 0
 total_fps = 0
-Generator.execute = True #Start next execution
+
+bbeginning = time.time() #For final FPS
 
 try:
   while True:
 
     beginning = time.time() #For FPS calculations
     
-    if Generator.execute: print("waiting...") # Wait for top thread to be ready
-    while Generator.execute: continue
-    
-    Generator.execute = True #Start next execution
-    
-    bbeginning = time.time()
-
-    ### predict boxes thread###
-    boxesThread = Process(target=boundingBoxes,
-                        args=(boxGenerator,
-                              Generator.current_edgearray,
-                              Generator.current_orientationarray,
-                              queue,),
-                        daemon = True)
-    boxesThread.start()
-    boxes = queue.get() #box[0] x1, box[1] x2, box[2] width, box[3] height
-    boxesThread.join()
-
-    print("BEx: ", round(time.time()-bbeginning, 3))
-    print("BBs: ", len(boxes))
+    boxes = Predictor.boxes.get()
 
     fps = 1/(time.time()-beginning)
     print("FPS: ", fps, "\n")
@@ -77,7 +53,7 @@ try:
 
     if video.frame is None:
         print("End of Video File!")
-        exit(total_fps/frames)
+        exit(frames/(time.time()-bbeginning))
 
 except KeyboardInterrupt:
-    exit(total_fps/frames)
+    exit(frames/(time.time()-bbeginning))
